3a821c5961450e8f0e54febc36ed445123433841
Ericka_qiu <ericka.qiu@mail.utoronto.ca>
2024-11-15 18:24:47 -0500

    implement TranslatorInputBoundary and TranslatorInputData

--- src/main/java/use_case/translator/TranslatorInputBoundary.java
+++ src/main/java/use_case/translator/TranslatorInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.translator;
 
+/**
+ * Input Boundary for actions which are related to translate.
+ */
 public interface TranslatorInputBoundary {
+
+    /**
+     * Executes the Translator use case.
+     * @param translatorInputData the input data.
+     */
+    void execute(TranslatorInputData translatorInputData);
 }
--- src/main/java/use_case/translator/TranslatorInputData.java
+++ src/main/java/use_case/translator/TranslatorInputData.java
@@ -1,4 +1,26 @@
 package use_case.translator;
 
+/**
+ * The input data for the Translator Use case.
+ */
 public class TranslatorInputData {
+    private final String input;
+    private String inputLanguage;
+
+    public TranslatorInputData(String input) {
+        this.input = input;
+    }
+
+    public TranslatorInputData(String input, String inputLanguage) {
+        this.input = input;
+        this.inputLanguage = inputLanguage;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getInputLanguage() {
+        return inputLanguage;
+    }
 }
