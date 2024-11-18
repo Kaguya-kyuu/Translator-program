425ec61b0cef3e813d3d87d854d46116ca658f5c
caizixu1 <ca.cai@mail.utoronto.ca>
2024-11-18 11:52:29 -0500

    charles

--- src/main/java/interface_adapter/translate/TranslateViewModel.java
+++ src/main/java/interface_adapter/translate/TranslateViewModel.java
@@ -1,4 +1,35 @@
 package interface_adapter.translate;
 
-public class TranslateViewModel {
+import interface_adapter.ViewModel;
+
+/**
+ * The ViewModel for Translator.
+ */
+
+public class TranslateViewModel extends ViewModel<TranslateState> {
+
+    public static final String TITLE_LABEL = "Translator";
+    public static final String LOGOUT_LABEL = "Log out";
+
+    public static final String INPUT_LANGUAGE_LABEL = "Choose your inout language";
+    public static final String OUTPUT_LANGUAGE_LABEL = "Choose your output language";
+
+    public static final String INPUT_TEXT_LABEL = "Input your text here";
+    public static final String OUTPUT_TEXT_LABEL = "Your translation will be here";
+
+    public static final String TRANSLATE_F_LABEL = "Translate";
+    public static final String OTHER_TRANSLATION_F_LABEL = "Others";
+
+    public static final String HISTORY_LABEL = "History";
+
+    public static final String BOOKMARK_F_LABEL = "Bookmark current translation";
+    public static final String BOOKMARKS_LABEL = "Bookmarks";
+
+    public static final String FEEDBACK_F_LABEL = "Submit a feedback";
+
+    public TranslateViewModel() {
+        super("translate");
+        setState(new TranslateState());
+    }
+
 }
